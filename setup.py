8246c5f881420263db24eeecb1e2bdf49d3b64ab
Anil Zenginoglu <anil.zenginoglu@gmail.com>
2020-08-31 10:17:23 -0400

    Enter missing info in setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="centpy-YOUR-USERNAME-HERE",  ############
+    name="centpy-anilzen",
     version="0.1",
     author="Anil Zenginoglu",
     author_email="anil@umd.edu",
     description="A numerical solver for conservation laws based on central schemes",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",  ############
+    url="https://github.com/AnilZen/centpy",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
